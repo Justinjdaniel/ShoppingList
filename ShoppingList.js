78ea992a536623012c09f558ad7eacb62c9b8667
Justin J Daniel <62233773+Justinjdaniel@users.noreply.github.com>
2020-05-03 16:01:34 +0530

    Stable V1.0 Finalized
    All commented unnecessary codes are removed bugs and mistakes are rectified

--- ShoppingList.js
+++ ShoppingList.js
@@ -5,13 +5,11 @@ function GroceryList(){
 
         if (this.readyState == 4&&this.status==200){
             var list = JSON.parse(this.responseText);
-            console.log(list);
+            // console.log(list);
             var slist = list.ShoppingList;
-            console.log(slist);
-            // document.getElementById("tabstart").innerHTML="<table><caption><h3>Shopping List</h1></caption>"
-            var tabstart = "<caption><h3>Shopping List</h1></caption> <tr><th>Sl. No</th><th>Particulars</th><th>Quantity</th><th>Unit Cost</th><th>Department</th><th>Notes</th></tr>"
-            console.log(tabstart);
-
+            // console.log(slist);
+            var tabstart = "<caption><h3>Shopping List</h1></caption> <tr><th>Sl. No</th><th>Particulars</th><th>Quantity</th><th>Unit Cost</th><th>Department</th><th>Notes</th></tr>";
+            // console.log(tabstart);
             for (var i=0;i<slist.length;i++){
                 tabstart += "<tr><td>"+ slist[i].SlNo +"</td>"+
                 "<td>"+ slist[i].Particulars +"</td>"+
