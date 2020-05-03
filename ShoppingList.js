afab95e053a3be2b1248f04925401fff7e58b4eb
Justin J Daniel <62233773+Justinjdaniel@users.noreply.github.com>
2020-05-03 15:58:16 +0530

    Bug Fixes & Minor Improvements

--- ShoppingList.js
+++ ShoppingList.js
@@ -1,19 +1,28 @@
-function ShopplingList(){
+function GroceryList(){
+
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function(){
-        if (this.readyState==4&&this.status==200){
+
+        if (this.readyState == 4&&this.status==200){
             var list = JSON.parse(this.responseText);
-            // console.log(list);
-            var Slist = list.ShoppingList;
-            // console.log(Slist);
+            console.log(list);
+            var slist = list.ShoppingList;
+            console.log(slist);
             // document.getElementById("tabstart").innerHTML="<table><caption><h3>Shopping List</h1></caption>"
-            var tabstart = "<table><caption><h3>Shopping List</h1></caption> <tr><th>Sl. No</th><th>Particulars</th><th>Quantity</th><th>Unit Cost</th><th>Department</th><th>Notes</th></tr>"
-            var output ="";
-            for (var i=0;i<Slist.lenght;i++){
-                output += "<tr><td>"+ Slist[i].SlNo +"</td>"+"<td>"+ Slist[i].Particulars +"</td>"+"<td>"+ Slist[i].Quantity +"</td>"+"<td>"+ Slist[i].UnitCost +"</td>"+"<td>"+ Slist[i].Department +"</td>"+"<td>"+ Slist[i].Notes +"</td></tr>";
+            var tabstart = "<caption><h3>Shopping List</h1></caption> <tr><th>Sl. No</th><th>Particulars</th><th>Quantity</th><th>Unit Cost</th><th>Department</th><th>Notes</th></tr>"
+            console.log(tabstart);
+
+            for (var i=0;i<slist.length;i++){
+                tabstart += "<tr><td>"+ slist[i].SlNo +"</td>"+
+                "<td>"+ slist[i].Particulars +"</td>"+
+                "<td>"+ slist[i].Quantity +"</td>"+
+                "<td>"+ slist[i].UnitCost +"</td>"+
+                "<td>"+ slist[i].Department +"</td>"+
+                "<td>"+ slist[i].Notes +"</td></tr>";
             }
-            document.getElementById("ShopList").innerHTML == tabstart + output + "</table>";
-            // document.getElementById("tabend").innerHTML ="</table>"
+            document.getElementById("table").innerHTML = tabstart;
+            let button = document.getElementById("ShopList");
+            button.setAttribute("hidden","true");
         }
     };
     xhttp.open("GET","ShoppingList.json",true);
